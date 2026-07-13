28dac56c2acc376fff309decc45e93375f17b8ef
Evan Owen <Johnwowen3@gmail.com>
2026-07-13 14:26:28 -0400

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        M, N = len(board), len(board[0])
+        MARK = '%'
+
+        def dfs(row: int, col: int, idx: int) -> bool:
+            if idx == len(word):
+                return True
+            if row < 0 or col < 0:
+                return False
+            if row >= M or col >= N:
+                return False
+            if board[row][col] == MARK:
+                return False
+            if word[idx] != board[row][col]:
+                return False
+            
+            board[row][col] = MARK
+            res = (
+                dfs(row + 1, col, idx + 1) or
+                dfs(row - 1, col, idx + 1) or
+                dfs(row, col + 1, idx + 1) or
+                dfs(row, col - 1, idx + 1)
+            )
+            board[row][col] = word[idx]
+            return res
+        
+        for r in range(M):
+            for c in range(N):
+                if dfs(r, c, 0):
+                    return True
+        return False
